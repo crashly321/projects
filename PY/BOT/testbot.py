faa6c1d15671f7e0c1c87540cb5079ab2330c0fa
crashly(home) <149060523+crashly321@users.noreply.github.com>
2025-02-06 19:14:48 +0300

    121

---
+++ PY/BOT/testbot.py
@@ -0,0 +1,14 @@
+import telebot
+
+bot = telebot.TeleBot('7575636058:AAGYv7xtuCrY6hR2KS-uq4J5wI68AphgeEQ')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text in ['Привет', 'привет']:
+        bot.send_message(message.from_user.id, 'Прив, можешь написать /help')
+    elif message.text in ['/help']:
+        bot.send_message(message.from_user.id, 'Пока что разраб(уебан) разрешил мне отвечать только на приветы')
+    else:
+        bot.send_message(message.from_user.id, 'Хз че тебе сказать')
+
+bot.polling(none_stop=True, interval=0)
