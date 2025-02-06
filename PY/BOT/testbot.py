8e1c60402ca37cf4b36c58257c115888472d8b87
cry.ashly <malimama142@gmail.com>
2025-02-06 19:20:29 +0300

    123

--- PY/BOT/testbot.py
+++ PY/BOT/testbot.py
@@ -1,5 +1,5 @@
 import telebot
-
+# python3 /Users/cryashly/Documents/gitRepositories/Codein/projects/PY/BOT/testbot.py
 bot = telebot.TeleBot('7575636058:AAGYv7xtuCrY6hR2KS-uq4J5wI68AphgeEQ')
 
 @bot.message_handler(content_types=['text'])
@@ -9,6 +9,6 @@ def get_text_messages(message):
     elif message.text in ['/help']:
         bot.send_message(message.from_user.id, 'Пока что разраб(уебан) разрешил мне отвечать только на приветы')
     else:
-        bot.send_message(message.from_user.id, 'Хз че тебе сказать')
+        bot.send_message(message.from_user.id, 'Хз че тебе сказать, отвечаю ток на привет')
 
 bot.polling(none_stop=True, interval=0)
